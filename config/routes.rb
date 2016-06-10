70578d660d933c4812df7d1209b6704745f66d96
Dominik Teipel <teipel-dominik@web.de>
2016-06-10 13:57:50 +0200

    welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  #root 'welcome#index'
+
  # resources :messages
  # resources :users
 
@@ -24,7 +27,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
